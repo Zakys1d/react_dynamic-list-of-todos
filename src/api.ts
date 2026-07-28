cef3d70368539f09397f4e496e3788b2e5d4b7ef
Maksym Huk <maksym.h1009@gmail.com>
2026-07-28 22:01:25 +0300

    some_bugs

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,20 @@
 import { Todo } from './types/Todo';
 import { User } from './types/User';
 
-// eslint-disable-next-line operator-linebreak
-const BASE_URL =
-  'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
+const BASE_URL = 'https://mate.academy/students-api';
 
-// This function creates a promise
-// that is resolved after a given delay
-function wait(delay: number): Promise<void> {
-  return new Promise(resolve => {
-    setTimeout(resolve, delay);
-  });
+function wait(delay: number) {
+  return new Promise(resolve => setTimeout(resolve, delay));
 }
 
-function get<T>(url: string): Promise<T> {
-  // eslint-disable-next-line prefer-template
-  const fullURL = BASE_URL + url + '.json';
-
-  // we add some delay to see how the loader works
+export function getTodos(): Promise<Todo[]> {
   return wait(300)
-    .then(() => fetch(fullURL))
+    .then(() => fetch(`${BASE_URL}/todos`))
     .then(res => res.json());
 }
 
-export const getTodos = () => get<Todo[]>('/todos');
-
-export const getUser = (userId: number) => get<User>(`/users/${userId}`);
+export function getUser(userId: number): Promise<User> {
+  return wait(300)
+    .then(() => fetch(`${BASE_URL}/users/${userId}`))
+    .then(res => res.json());
+}
